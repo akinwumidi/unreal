f35f94f392815da4dd164e60783b7b2120db5774
Akinwumidi <israelakin14@gmail.com>
2023-08-22 18:23:23 +0100

    Header section: optimized for smaller devices

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var menu = document.querySelector(".navmobile-menu")
 var mobilelinks = document.querySelector(".mobile-navigations")
+var backdrop = document.querySelector(".backdrop")
 
 function hamburgerHandler() {
     menu.classList.toggle('showMenu')
+    backdrop.classList.toggle('show-backdrop')
     mobilelinks.classList.toggle('show-mobile-navigations')
 }
